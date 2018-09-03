9372eaeea65b71b34626d13d3d85c3f4ba123a52
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-03 20:50:30 -0300

    Configure db connection, middlewares and routes

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+global.connection = require('./database');
+
 // Settings
-app.set('port', process.env.PORT || 3000);
-;
+app.set('port', process.env.PORT || 80); 
+
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 // Routes
+app.use('/api/structure', require('./routes/structure.routes'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
