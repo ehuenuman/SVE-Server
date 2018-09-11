b64bed684f631fccf1c03d926ea13ecda79963b4
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-11 12:17:10 -0300

    Install and config express-session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const uuid = require('uuid/v4');
+const session = require('express-session');
 
 // Create server
 const app = express();
@@ -12,13 +13,23 @@ app.set('port', process.env.PORT || 80);
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(session({
+  genid:  (req) => {
+    console.log('Inside the session middleware');
+    console.log(req.sessionID);
+    return uuid();
+  },
+  secret: 'mota floja',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // Routes
 app.get('/', (req, res) => {
-    //console.log(req);
-    const uniqueId = uuid();
-    //console.log(uniqueId);
-    res.send('Hit home page. Recieved the unique id: ${uniqueId}');
+  console.log('Inside the homepage callback function');
+  console.log(req.sessionID);
+  //console.log(uniqueId);
+  res.send('Ypu hit home page!');
 });
 app.use('/api/structure', require('./routes/structure.routes'));
 app.use('/api/sensor', require('./routes/sensor.routes'));
@@ -28,5 +39,5 @@ global.connection = require('./config/database');
 
 // Starting the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port', app.get('port'));
+  console.log('Server on port', app.get('port'));
 });
