b9d76edbd65fb93085bc3c11f3d9e91d0fff1aa8
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-12 15:19:57 -0300

    Add process for login

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router  = express.Router();
 
-const controller = require('../controllers/login.controller')
+//const controller = require('../controllers/login.controller')
+const passport = require('../config/passport');
 
-router.post('/login', controller.loginUser);
+router.post('/login', (req, res, next) => {
+  console.log('Inside POST /login callback function');
+  console.log(req.body);
+  passport.authenticate('login', (err, user, info) => {
+    console.log('Inside passport.authenticate() callback');
+    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+    console.log(`req.user: ${JSON.stringify(req.user)}`)
+    req.login(user, (err) => {
+      console.log('Inside req.login() callback')
+      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      console.log(`req.user: ${JSON.stringify(req.user)}`)
+      return res.send('You were authenticated & logged in!\n');
+    })
+  })(req, res, next);
+});
 
 module.exports = router;
