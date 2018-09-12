2fdeb83b00c93f0fb5d4574989caf32ba8025a44
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-12 15:17:48 -0300

    Install body-parser

--- index.js
+++ index.js
@@ -2,15 +2,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const uuid = require('uuid/v4');
-const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-//const passport = require('passport');
-//const LocalStrategy = require('passport-local').Strategy;
 
-// Passport configuration
-//require('./config/passport')(passport);
-//global.passport = require('./config/passport');
 const passport = require('./config/passport');
 
 // Create server
@@ -21,13 +16,13 @@ app.set('port', process.env.PORT || 80);
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(session({
   genid: (req) => {
     console.log('Inside the session middleware');
     console.log(req.sessionID);
-    return uuid();
+    return uuid();  
   },
   store: new FileStore(),
   secret: 'mota floja',
@@ -41,7 +36,7 @@ app.use(passport.session());
 app.get('/', (req, res) => {
   console.log('Inside the homepage callback function');
   console.log(req.sessionID);
-  res.send('You hit home page!');
+  res.send("HomePage");
 });
 app.use('/auth', require('./routes/login.routes'));
 app.use('/api/structure', require('./routes/structure.routes'));
