88793b1c866694f65862553e1a84e17f1ea68948
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-13 16:36:43 -0300

    Change response for json object

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -7,7 +7,7 @@ const passport = require('../config/passport');
 router.post('/login', (req, res, next) => {
   console.log('Inside POST /login callback function');
   console.log(req.body);
-  passport.authenticate('login', (err, user, info) => {
+  passport.authenticate('login', (error, user, info) => {
     console.log('Inside passport.authenticate() callback');
     console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
     console.log(`req.user: ${JSON.stringify(req.user)}`)
@@ -15,7 +15,11 @@ router.post('/login', (req, res, next) => {
       console.log('Inside req.login() callback')
       console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
       console.log(`req.user: ${JSON.stringify(req.user)}`)
-      return res.send('You were authenticated & logged in!\n');
+      return res.json({
+        'error': error,
+        'info': info.msg,
+        'user': req.user
+      })
     })
   })(req, res, next);
 });
