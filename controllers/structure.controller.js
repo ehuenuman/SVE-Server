099c84849b81328a9b29ed8c8e803dac78e840e7
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-16 23:11:42 -0300

    Add routes and controller for Guest and "Responsable" users

--- controllers/structure.controller.js
+++ controllers/structure.controller.js
@@ -1,18 +1,60 @@
 const structureController = {};
 
-structureController.getStructures = (req, res) => {
-    connection.query('call getStructuresAdmin', function (error, results, fields) {
-        if (error) {
-            res.json({
-                "status": 500, "error": error, "repsonse": null
-            });
-        } else {
-            res.json({
-                "status": 200, "error": null, "response": results
-            });
-        }
-    });
-}
+structureController.getStructuresForAdmin = (req, res) => {
+  connection.query('call getStructuresAdmin', function (error, results, fields) {
+    if (error) {
+      res.json({
+        "status": 500, "error": error, "repsonse": null
+      });
+    } else {
+      res.json({
+        "status": 200, "error": null, "response": results
+      });
+    }
+  });
+};
+
+structureController.getStructuresOfRepr = (req, res) => {
+  connection.query("call getStructuresOfRepr("+ req.user.enterprise_id +")", function (error, result, fields) {
+    if (error) {
+      res.json({
+        "status": 500, "error": error, "response": null
+      });
+    } else {
+      res.json({
+        "status": 200, "error": null, "response": result
+      });
+    }
+  });
+};
+
+structureController.getStructureOfResp = (req, res) => {
+  connection.query("call getStructuresOfResp(?)", req.user.user_id, function (error, result, fields) {
+    if (error) {
+      res.json({
+        "status": 500, "error": error, "response": null
+      });
+    } else {
+      res.json({
+        "status": 200, "error": null, "response": result
+      });
+    }
+  });
+};
+
+structureController.getStructuresOfGuest = (req, res) => {
+  connection.query("call getStructuresOfRepr(?)", req.user.enterprise_id, function (error, result, fields) {
+    if (error) {
+      res.json({
+        "status": 500, "error": error, "response": null
+      });
+    } else {
+      res.json({
+        "status": 200, "error": null, "response": result
+      });
+    }
+  });
+};
 
 structureController.getStructure = function() {}
 
@@ -21,6 +63,5 @@ structureController.createStructure = function() {}
 structureController.editStructure = function() {}
 
 structureController.deleteStructure = function() {}
-
 
 module.exports = structureController;
--- routes/structure.routes.js
+++ routes/structure.routes.js
@@ -3,13 +3,37 @@ const router  = express.Router();
 
 const controller = require('../controllers/structure.controller')
 
+/* Tipos de usuarios
+ * 1: Administrador
+ * 2: Representante
+ * 3: Responsable
+ * 4: Invitado
+ */
+
 router.get('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    controller.getStructures
+  switch ( req.user.profile_id ) {
+    case 1:
+      controller.getStructuresForAdmin(req, res);
+      break;
+    case 2:
+      controller.getStructuresOfRepr(req, res);
+      break;
+    case 3:
+      controller.getStructureOfResp(req, res);
+      break;
+    case 4:
+      controller.getStructuresOfGuest(req, res);
+      break;
+  }
+}
+  
+  /*if (req.isAuthenticated()) {
+    controller.getStructuresForAdmin
   } else {
     res.redirect('/');
-  }
-});
+  }*/
+);
+
 router.get('/:id', controller.getStructure);
 router.post('/', controller.createStructure);
 router.put('/:id', controller.editStructure);
