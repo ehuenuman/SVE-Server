833cdb12c95b45c76ba8acb61b8091195cf3a313
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-20 18:35:23 -0300

    Delete package not used and handler of 401 error

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 // npm modules
 const express = require('express');
 const morgan = require('morgan');
-const uuid = require('uuid/v4');
 const bodyParser = require('body-parser');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
 
 const passport = require('./config/passport');
 
@@ -18,29 +15,21 @@ app.set('port', process.env.PORT || 80);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(session({
-  genid: (req) => {
-    console.log('Inside the session middleware');
-    console.log(req.sessionID);
-    return uuid();  
-  },
-  store: new FileStore(),
-  secret: 'mota floja',
-  resave: false,
-  saveUninitialized: true
-}));
 app.use(passport.initialize());
-app.use(passport.session());
 
 // Routes
-app.get('/', (req, res) => {
-  console.log('Inside the homepage callback function');
-  console.log(req.sessionID);
-  res.send("HomePage");
-});
-app.use('/auth', require('./routes/login.routes'));
+app.use('/auth', require('./routes/auth.routes'));
 app.use('/api/structure', require('./routes/structure.routes'));
 app.use('/api/sensor', require('./routes/sensor.routes'));
+
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401);
+    res.json({
+      "msg": err.name + ": " + err.message
+    });
+  }
+});
 
 // Database connection
 global.connection = require('./config/database');
