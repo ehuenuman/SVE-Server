7d83826695cc895aba5c7e8bd44dc866fd3347ca
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-24 23:41:28 -0300

    Create get sensors without alert

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -1,6 +1,19 @@
+const methods = require('./methods.controller');
+
 const sensorController = {};
 
-sensorController.getSensors = function() {}
+sensorController.getSensors = (req, res) => {
+  connection.query("call getSensorsOfStructure(" + req.body.structure_id + ")", function (error, result, fields) {
+    if (!methods.isError(error, res)) {
+      res.status(200);
+      res.json({
+        "status": 200,
+        "error": null,
+        "response": result[0]
+      });
+    }
+  });
+}
 
 sensorController.getSensor = function() {}
 
