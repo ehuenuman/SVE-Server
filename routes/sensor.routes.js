6eb006b673419f1d96ea774f78a80d46e8009613
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-24 23:43:18 -0300

    create route for getSensors

--- routes/sensor.routes.js
+++ routes/sensor.routes.js
@@ -3,14 +3,20 @@ const router  = express.Router();
 
 const controller = require('../controllers/sensor.controller')
 
-router.get('/', (req, res) => {
-  if (req.isAuthenticated()) {
-    //controller.getSensors    
-    res.send('Sensor');
-  } else {
-    res.redirect('/');
+var jwt = require('express-jwt');
+var auth = jwt({
+  secret: "MY_SECRET",
+  getToken: function fromHeaderOrQueryString (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      return req.query.token;
+    }
+    return null;
   }
 });
+
+router.post('/', auth, controller.getSensors);
 router.get('/:id', controller.getSensor);
 router.post('/', controller.createSensor);
 router.put('/:id', controller.editSensor);
