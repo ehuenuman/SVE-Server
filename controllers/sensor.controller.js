f9fc5e3fbdc07253f986e43d9bde7f797435a344
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-28 13:12:35 -0300

    Se escribe el procedimiento para obtener el sensor de una estructura verificando si es correcta o no

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -15,10 +15,28 @@ sensorController.getSensors = (req, res) => {
   });
 }
 
-sensorController.getSensor = (req, res) => {
-  res.status(200);
-  res.json({
-    "status": 200
+sensorController.getSensorOfStructure = (req, res) => {
+  let structure_id = req.params.structure_id;
+  let sensor_id = req.params.sensor_id;
+
+  connection.query("call getSensorOfStructure(" + sensor_id + "," + structure_id + ")", function(error, result, fields) {
+    if (!methods.isError(error, res)) {
+      if (result[0].length == 0) {
+        res.status(404);
+        res.json({
+          "status": 404,
+          "error": "La estructura no utiliza el sensor.",
+          "response": []
+        });
+      } else {
+        res.status(200);
+        res.json({
+          "status": 200,
+          "error": null,
+          "response": result[0]
+        });
+      }
+    }
   });
 }
 
