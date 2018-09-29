32e774aa735e4902cf6f3f2b7b14896a1a49647b
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-09-29 13:33:16 -0300

    Se procesa el resultado de la consulta antes de enviar al cliente.

--- controllers/sensor.controller.js
+++ controllers/sensor.controller.js
@@ -5,11 +5,34 @@ const sensorController = {};
 sensorController.getSensors = (req, res) => {
   connection.query("call getSensorsOfStructure(" + req.params.id + ")", function (error, result, fields) {        
     if (!methods.isError(error, res)) {
+      var sensors = [];
+      var data = result[0];      
+      data.forEach(sensor => {
+        if (sensors.length == 0) {
+          sensors.push(sensor);
+        } else {
+          var id = sensor.id;
+          var adv_count = sensor.adv_count;
+          var ale_count = sensor.ale_count;
+          var temp_sensor = sensors[sensors.length-1]
+          if (temp_sensor.id == id) {
+            if (temp_sensor['adv_count'] > 0 && temp_sensor['ale_count'] == 0) {
+              sensors[sensors.length-1]['ale_count'] = ale_count;
+            } 
+            if (temp_sensor['adv_count'] == 0 && temp_sensor['ale_count'] > 0) {
+              sensors[sensors.length-1]['adv_count'] = adv_count;
+            }
+          } else {
+            sensors.push(sensor);
+          }
+        }
+      });
+
       res.status(200);
       res.json({
         "status": 200,
         "error": null,
-        "response": result[0]
+        "response": sensors
       });
     }
   });
@@ -33,7 +56,7 @@ sensorController.getSensorOfStructure = (req, res) => {
         res.json({
           "status": 200,
           "error": null,
-          "response": result[0]
+          "response": result[0][0]
         });
       }
     }
