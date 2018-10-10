8313275567102f32d3ba9fb980a2d63422920aa8
Esteban Huenumán <esteban.huenuman@hotmail.cl>
2018-10-10 12:19:05 -0300

    Se agrega el procedimiento que maneja la respuesta desde el servidor.

--- controllers/structure.controller.js
+++ controllers/structure.controller.js
@@ -3,11 +3,36 @@ const methods = require('./methods.controller');
 const structureController = {};
 
 structureController.getStructuresOfAdmin = (req, res) => {
-  mysql.query('call getStructuresAdmin', function (error, results, fields) {
+  mysql.query('call getStructuresAdmin', function (error, result, fields) {
     if (!methods.isError(error, res)) {
+      var structures = [];
+      var data = result[0];
+      data.forEach(structure => {
+        if (structures.length == 0) {
+          structures.push(structure);
+        } else {
+          var id = structure.id;
+          var adv_count = structure.adv_count;
+          var ale_count = structure.ale_count;
+          var temp_sensor = structures[structures.length-1]
+          if (temp_sensor.id == id) {
+            if (temp_sensor['adv_count'] > 0 && temp_sensor['ale_count'] == 0) {
+              structures[structures.length-1]['ale_count'] = ale_count;
+            } 
+            if (temp_sensor['adv_count'] == 0 && temp_sensor['ale_count'] > 0) {
+              structures[structures.length-1]['adv_count'] = adv_count;
+            }
+          } else {
+            structures.push(structure);
+          }
+        }
+      });
+      
       res.status(200);
       res.json({
-        "status": 200, "error": null, "response": results
+        "status": 200, 
+        "error": null, 
+        "response": structures
       });
     }
   });
@@ -16,9 +41,34 @@ structureController.getStructuresOfAdmin = (req, res) => {
 structureController.getStructuresOfRepr = (req, res) => {
   mysql.query("call getStructuresOfRepr(" + req.user.enterprise_id + ")", function (error, result, fields) {
     if (!methods.isError(error, res)) {
+      var structures = [];
+      var data = result[0];
+      data.forEach(structure => {
+        if (structures.length == 0) {
+          structures.push(structure);
+        } else {
+          var id = structure.id;
+          var adv_count = structure.adv_count;
+          var ale_count = structure.ale_count;
+          var temp_sensor = structures[structures.length-1]
+          if (temp_sensor.id == id) {
+            if (temp_sensor['adv_count'] > 0 && temp_sensor['ale_count'] == 0) {
+              structures[structures.length-1]['ale_count'] = ale_count;
+            } 
+            if (temp_sensor['adv_count'] == 0 && temp_sensor['ale_count'] > 0) {
+              structures[structures.length-1]['adv_count'] = adv_count;
+            }
+          } else {
+            structures.push(structure);
+          }
+        }
+      });
+      
       res.status(200);
       res.json({
-        "status": 200, "error": null, "response": result
+        "status": 200, 
+        "error": null, 
+        "response": structures
       });
     }
   });
@@ -27,9 +77,34 @@ structureController.getStructuresOfRepr = (req, res) => {
 structureController.getStructureOfResp = (req, res) => {
   mysql.query("call getStructuresOfResp(" + req.user._id + ")", function (error, result, fields) {
     if (!methods.isError(error, res)) {
+      var structures = [];
+      var data = result[0];
+      data.forEach(structure => {
+        if (structures.length == 0) {
+          structures.push(structure);
+        } else {
+          var id = structure.id;
+          var adv_count = structure.adv_count;
+          var ale_count = structure.ale_count;
+          var temp_sensor = structures[structures.length-1]
+          if (temp_sensor.id == id) {
+            if (temp_sensor['adv_count'] > 0 && temp_sensor['ale_count'] == 0) {
+              structures[structures.length-1]['ale_count'] = ale_count;
+            } 
+            if (temp_sensor['adv_count'] == 0 && temp_sensor['ale_count'] > 0) {
+              structures[structures.length-1]['adv_count'] = adv_count;
+            }
+          } else {
+            structures.push(structure);
+          }
+        }
+      });
+      
       res.status(200);
       res.json({
-        "status": 200, "error": null, "response": result
+        "status": 200, 
+        "error": null, 
+        "response": structures
       });
     }
   });
@@ -38,9 +113,34 @@ structureController.getStructureOfResp = (req, res) => {
 structureController.getStructuresOfGuest = (req, res) => {
   mysql.query("call getStructuresOfRepr(" + req.user.enterprise_id + ")", function (error, result, fields) {
     if (!methods.isError(error, res)) {
+      var structures = [];
+      var data = result[0];
+      data.forEach(structure => {
+        if (structures.length == 0) {
+          structures.push(structure);
+        } else {
+          var id = structure.id;
+          var adv_count = structure.adv_count;
+          var ale_count = structure.ale_count;
+          var temp_sensor = structures[structures.length-1]
+          if (temp_sensor.id == id) {
+            if (temp_sensor['adv_count'] > 0 && temp_sensor['ale_count'] == 0) {
+              structures[structures.length-1]['ale_count'] = ale_count;
+            } 
+            if (temp_sensor['adv_count'] == 0 && temp_sensor['ale_count'] > 0) {
+              structures[structures.length-1]['adv_count'] = adv_count;
+            }
+          } else {
+            structures.push(structure);
+          }
+        }
+      });
+      
       res.status(200);
       res.json({
-        "status": 200, "error": null, "response": result
+        "status": 200, 
+        "error": null, 
+        "response": structures
       });
     }
   });
